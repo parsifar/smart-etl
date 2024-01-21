9933be4201bfbc0c7b5e40ef467164a34ed5d6ea
aparsifar <aparsifar@rcdesign.com>
2024-01-21 11:21:34 -0500

    update copyright text

--- functions.php
+++ functions.php
@@ -4,4 +4,19 @@
  *
  * Add your custom PHP in this file.
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
+ *
+ * @package smart-etl
  */
+
+/**
+ * Change Copyright Text
+ *
+ * @return void
+ */
+function smart_etl_custom_copyright() {
+	?>
+	© <?php echo esc_attr( date( 'Y' ) ); ?> <?php echo esc_attr( get_bloginfo( 'name' ) ); ?>. Developed by <a href="https://parsifar.com" target="_blank">Ali Parsifar</a>
+	<?php
+}
+
+add_filter( 'generate_copyright', 'smart_etl_custom_copyright' );
